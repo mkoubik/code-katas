6e840d0a232daea36a53ec74b186be68ad3d0085
Matej Koubik <matej@koubik.name>
2013-06-05 12:17:39 +0200

    green

--- 2013-06-15-string-calculator/src/Calculator.php
+++ 2013-06-15-string-calculator/src/Calculator.php
@@ -10,6 +10,11 @@ class Calculator {
 		}
 		$string = str_replace("\n", $delimiter, $string);
 		$numbers = explode($delimiter, $string);
+		array_walk($numbers, function($number) {
+			if ($number < 0) {
+				throw new Exception("Negatives not allowed: $number");
+			}
+		});
 		return array_sum($numbers);
 	}
 }
