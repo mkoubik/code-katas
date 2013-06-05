51be5ffe5cbda38dc331bbba122f298a12b9da57
Matej Koubik <matej@koubik.name>
2013-06-05 12:03:27 +0200

    green

--- 2013-06-15-string-calculator/src/Calculator.php
+++ 2013-06-15-string-calculator/src/Calculator.php
@@ -3,13 +3,7 @@
 class Calculator {
 	public function add($string)
 	{
-		if ($string === '') {
-			return 0;
-		}
 		$numbers = explode(',', $string);
-		if (count($numbers) === 2) {
-			return $numbers[0] + $numbers[1];
-		}
-		return (int) $string;
+		return array_sum($numbers);
 	}
 }
