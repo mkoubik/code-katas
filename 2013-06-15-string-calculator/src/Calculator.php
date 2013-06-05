7069c50b5968d5ba635a0c569e8e1a33428b5cc0
Matej Koubik <matej@koubik.name>
2013-06-05 12:11:26 +0200

    green

--- 2013-06-15-string-calculator/src/Calculator.php
+++ 2013-06-15-string-calculator/src/Calculator.php
@@ -3,6 +3,10 @@
 class Calculator {
 	public function add($string)
 	{
+		if (strpos($string, '//') === 0) {
+			$numbers = explode(substr($string, 2, 1), substr($string, 4));
+			return array_sum($numbers);
+		}
 		$string = str_replace("\n", ',', $string);
 		$numbers = explode(',', $string);
 		return array_sum($numbers);
